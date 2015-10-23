6678e13d0325ee3a361026ab329c31051b1eafab
Maxime Lebastard <maxime.lebastard@etudiant.univ-rennes1.fr>
2015-10-23 15:15:07 +0200

    Add FXML and view

--- src/main/java/fr/istic/aoc/metronome/view/MetronomeViewImpl.java
+++ src/main/java/fr/istic/aoc/metronome/view/MetronomeViewImpl.java
@@ -1,8 +1,29 @@
 package fr.istic.aoc.metronome.view;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  *
  * Created by leiko on 23/10/15.
  */
-public class MetronomeViewImpl {
+public class MetronomeViewImpl implements Initializable{
+
+    @FXML
+    public Circle led_led1;
+
+    @FXML
+    public Button bt_start;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        led_led1.setFill(Color.GREEN);
+    }
 }
